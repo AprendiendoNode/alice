83bb03168e20b54c1f77273b25a659bc91f37afa
Esquinca <esquinca_jose@hotmail.com>
2019-07-17 09:15:25 -0500

    Fix comando banxico
    Funciona a como al 100

--- app/Console/Commands/enchangeratefix.php
+++ app/Console/Commands/enchangeratefix.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Base\ExchangeRate;
+use Carbon\Carbon;
 use DB;
 
 class enchangeratefix extends Command
@@ -30,31 +31,75 @@ class enchangeratefix extends Command
     public function handle()
     {
       $result = ExchangeRate::getExchangeRateFix();
-
-      dd($result);
-
+      $mutable = Carbon::now();
+      // $mutable = Carbon::create(2019, 7, 23, 0);
+      // $mutable = Carbon::create(2019, 7, 13, 0)->format('Y-m-d');
+      // $friday = $mutable->subDays(3)->format('Y-m-d');
+      
       $data = $result['bmx']['series'];
       $series = $data[0]['datos'];
 
-      $current_date = $series[0]['fecha'];
-      $current_date = str_replace('/', '-', $current_date );
-      $current_date = date("Y-m-d", strtotime($current_date));
+      $banxico_date = $series[0]['fecha'];
+      $banxico_date = str_replace('/', '-', $banxico_date );
+      $banxico_date_string = date("Y-m-d", strtotime($banxico_date));
 
-      $current_rate = $series[0]['dato'];
-      $code_banxico =$data[0]['idSerie'];
+      $banxico_carbon = Carbon::parse($banxico_date);
+      $banxico_carbon_string = Carbon::parse($banxico_date)->format('Y-m-d');
 
-      DB::table('exchange_rates')->insert([
-        'current_date' => $current_date,
+      switch ($mutable) {
+        case $mutable->isSaturday():
+          $latest = DB::table('exchange_rates')->latest()->first();
+          $fix_rate = 0.0000;
+          $dof_rate = 0.0000;
+          $current_rate = $latest->current_rate_dof;
+          $code_banxico =$data[0]['idSerie'];
+          break;
+        case $mutable->isSunday():
+          $latest = DB::table('exchange_rates')->latest()->first();
+          $fix_rate = 0.0000;
+          $dof_rate = 0.0000;
+          $current_rate = $latest->current_rate;
+          $code_banxico =$data[0]['idSerie'];
+          break;
+        case $mutable->isMonday():
+          $friday = $mutable->subDays(3)->format('Y-m-d');
+          $latest = DB::table('exchange_rates')->where('current_date', $friday)->first();
+          $fix_rate = $series[0]['dato'];
+          $dof_rate = $latest->current_rate_fix;
+          $current_rate = $latest->current_rate_dof;
+          $code_banxico =$data[0]['idSerie'];
+          break;
+        default:
+          $latest = DB::table('exchange_rates')->latest()->first();
+          $fix_rate = $series[0]['dato'];
+          $dof_rate = $latest->current_rate_fix;
+          $current_rate = $latest->current_rate_dof;
+          $code_banxico =$data[0]['idSerie'];
+          // dd('banxico api fix: ' . $fix_rate, 'dof_bd: ' . $latest->current_rate_dof, 'fix_bd: ' . $latest->current_rate_fix);
+          break;
+      }
+            
+      $data_insert = [
+        'current_date' => $banxico_date_string,
         'currency_id' => 2,
         'code_banxico' => $code_banxico,
-        'current_rate_fix' => $current_rate,
+        'current_rate_fix' => $fix_rate,
+        'current_rate_dof' => $dof_rate,
+        'current_rate' => $current_rate,
         'modified_rate' => $current_rate,
         'status' => 1,
         'created_uid' => 2,
         'updated_uid' => 2,
         'created_at' => \Carbon\Carbon::now()
-      ]);
-    }
+      ];
+      $this->insertExchange($data_insert);
+      $this->info('Datos insertados correctamente, comando completado.');
+      // dd($data_insert, 'Datos insertados.');
 
+    }
+    public function insertExchange($data)
+    {
+      DB::table('exchange_rates')->insert($data);
+    }
 
 }
