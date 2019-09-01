9f1d78c1f551713754e88256bd147843de37ec42
Unknown <jrsierrahernandez@gmail.com>
2019-09-01 16:15:48 -0500

    Modales de encuestas

--- resources/views/permitted/survey/dashboard.blade.php
+++ resources/views/permitted/survey/dashboard.blade.php
@@ -18,6 +18,96 @@
 
 @section('content')
   @if( auth()->user()->can('View dashboard survey nps') )
+    <div class="modal modal-default fade" id="modal-view-encuestas" data-backdrop="static">
+        <div class="modal-dialog" >
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h4 class="modal-title"><i class="fa fa-id-card-o" style="margin-right: 4px;"></i>Encuestas.</h4>
+            </div>
+            <div class="box-body table-responsive">
+            <div class="box-body">
+              <div class="row">
+                <div id="captura_pdf_general" class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                  <div class="row pad-top-botm client-info">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <p class="text-center" style="border: 1px solid #3D9970" >Información de encuestas.</p>
+                      <div class="clearfix">
+                        <table id="table_encuestas_boxes" class="table table-striped table-bordered table-hover">
+                          <thead>
+                            <tr>
+                              <th>Cliente</th>
+                              <th>Sitio</th>
+                              <th>Ing. Asignado</th>
+                              <th>Estado</th>
+                              <th>Calificación</th>
+                              <th>Fecha de registro</th>
+                            </tr>
+                          </thead>
+                          <tbody style="font-size: 11px;">
+
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+            <div class="modal-footer">
+              <!-- <button type="button" class="btn btn-primary btn-export"><i class="fa fa-save"></i> Exportar PDF</button> -->
+              <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times" style="margin-right: 4px;"></i>{{ trans('message.ccmodal') }}</button>
+            </div>
+          </div>
+        </div>
+    </div>
+
+
+    <div class="modal modal-default fade" id="modal-view-ppd" data-backdrop="static">
+        <div class="modal-dialog" >
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h4 class="modal-title"><i class="fa fa-id-card-o" style="margin-right: 4px;"></i>Calificaciones.</h4>
+            </div>
+            <div class="box-body table-responsive">
+            <div class="box-body">
+              <div class="row">
+                <div id="captura_pdf_general" class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                  <div class="row pad-top-botm client-info">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <p class="text-center" style="border: 1px solid #3D9970" >Calificaciones.</p>
+                      <div class="clearfix">
+                        <table id="table_boxes_ppd" class="table table-striped table-bordered table-hover">
+                          <thead>
+                            <tr>
+                              <th>Cliente</th>
+                              <th>Sitio</th>
+                              <th>Ing. Asignado</th>
+                              <th>Fecha de registro</th>
+                              <th>Calificación</th>
+                            </tr>
+                          </thead>
+                          <tbody style="font-size: 11px;">
+
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+            <div class="modal-footer">
+              <!-- <button type="button" class="btn btn-primary btn-export"><i class="fa fa-save"></i> Exportar PDF</button> -->
+              <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times" style="margin-right: 4px;"></i>{{ trans('message.ccmodal') }}</button>
+            </div>
+          </div>
+        </div>
+    </div>
+
     <div class="row">
       <div class="col-md-12 grid-margin-onerem  stretch-card">
         <div class="card">
@@ -118,7 +208,7 @@
         <div class="col-md-2">
           <div class="row">
             <div class="col-md-12 mb-3">
-              <div class="card">
+              <div class="card" id="box_promotores">
                 <div class="card-body">
                   <div class="d-xl-flex  align-items-center justify-content-center p-0 item">
                     <i class="mdi mdi-emoticon icon-lg mr-3 text-success"></i>
@@ -131,7 +221,7 @@
               </div>
             </div>
             <div class="col-md-12 mb-3">
-              <div class="card">
+              <div class="card" id="box_pasivos">
                 <div class="card-body">
                   <div class="d-xl-flex  align-items-center justify-content-center p-0 item">
                     <i class="mdi mdi-emoticon-neutral icon-lg mr-3 text-warning"></i>
@@ -144,7 +234,7 @@
               </div>
             </div>
             <div class="col-md-12 mb-3">
-              <div class="card">
+              <div class="card" id="box_detractores">
                 <div class="card-body">
                   <div class="d-xl-flex align-items-center justify-content-center p-0 item">
                     <i class="mdi mdi-emoticon-sad icon-lg mr-3 text-danger"></i>
