6fec1d16cb6f4d695a2c955890739045778da637
Unknown <jrsierrahernandez@gmail.com>
2019-09-02 09:09:28 -0500

    Ver reportes ok

--- app/Http/Controllers/Report/Basic.php
+++ app/Http/Controllers/Report/Basic.php
@@ -23,7 +23,7 @@ class Basic extends Controller
     $user_role = User::find($user_id)->getRoleNames();
 
     //$hotels = DB::select('CALL px_sitiosXusuario_rol(?, ?)', array(26, 'itconcierge'));
-    $hotels = DB::select('CALL px_sitiosXusuario_rol(?, ?)', array($user_id, "'".$user_role[0]."'"));
+    $hotels = DB::select('CALL px_sitiosXusuario_rol(?, ?)', array($user_id, $user_role[0].""));
 
     return view('permitted.report.view_reports', compact('hotels'));
   }
