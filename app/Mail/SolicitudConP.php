95d98992b7785396cbede16f3f21a06ab78fe3ff
Esquinca <esquinca_jose@hotmail.com>
2019-09-04 14:49:04 -0500

    Controllers Mails
    Controladores y mails de comandos missing.

---
+++ app/Mail/SolicitudConP.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SolicitudConP extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $param; //Variable publica
+//    public $param1; //Variable publica
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($param)
+    {
+        $this->param = $param;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Confirmación de pago.')->view('mail.solicitudConP_mod');
+        //return $this->view('view.name');
+    }
+}
