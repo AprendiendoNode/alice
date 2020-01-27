773b7a8ff4bdcded7a4b03bf58a9679596b907eb
Alonso Cauich <alonsocauichv1@gmail.com>
2020-01-27 13:42:53 -0600

    Actualización contable v1

--- resources/views/permitted/integration/rubros.blade.php
+++ resources/views/permitted/integration/rubros.blade.php
@@ -25,7 +25,27 @@
 
 @push('scripts')
   @if( auth()->user()->can('View rubros') )
+    <link rel="stylesheet" href="{{ asset('plugins/select2/dist/css/select2.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('plugins/select2/dist/css/select2-bootstrap.min.css') }}" type="text/css" />
+    <script src="{{ asset('plugins/select2/dist/js/select2.full.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('plugins/select2/dist/js/i18n/es-MX.js') }}" type="text/javascript"></script>
 
+    <link href="{{ asset('bower_components/bootstrap4-toggle-master/css/bootstrap4-toggle.min.css')}}" rel="stylesheet" type="text/css">
+    <script src="{{ asset('bower_components/bootstrap4-toggle-master/js/bootstrap4-toggle.min.js')}}"></script>
+
+    <link href="{{ asset('bower_components/datatables_bootstrap_4/datatables.min.css')}}" rel="stylesheet" type="text/css">
+    <script src="{{ asset('bower_components/datatables_bootstrap_4/datatables.min.js')}}"></script>
+
+    <script src="{{ asset('plugins/momentupdate/moment.js')}}"></script>
+    <link href="{{ asset('plugins/daterangepicker-master/daterangepicker.css')}}" rel="stylesheet" type="text/css">
+    <script src="{{ asset('plugins/daterangepicker-master/daterangepicker.js')}}"></script>
+
+    <script src="{{ asset('plugins/jquery-wizard-master-two/jquery.validate.min.js')}}"></script>
+    <script src="{{ asset('plugins/jquery-wizard-master-two/additional-methods.js')}}"></script>
+
+    <script src="{{ asset('bower_components/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js')}}"></script>
+
+    <script src="{{ asset('js/admin/integration/rubros.js')}}"></script>
   @else
   @endif
 @endpush
